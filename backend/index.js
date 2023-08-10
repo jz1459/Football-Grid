8ea9f0fdf04e89f4c680a894fd99fc50d7198ddf
Jason Zheng <jason.zheng@yale.edu>
2023-08-09 21:10:12 -0400

    First Commit with node backend and react frontend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+require('dotenv/config');
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, function () {
+    console.log(`Server started on port ${port}.`);
+});
